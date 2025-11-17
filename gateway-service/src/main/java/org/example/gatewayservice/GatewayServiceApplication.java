3c6f828e0e8b4421e6102f2cd1084114225f4416
MOHAMMED BOURHYM <simobourhym01@gmail.com>
2025-11-17 10:06:37 +0100

    Gatewat service set-up

--- gateway-service/src/main/java/org/example/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/org/example/gatewayservice/GatewayServiceApplication.java
@@ -2,8 +2,12 @@ package org.example.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
